834b10e8c0acb98ddd64abe9345804552843fccb
unknown <contacto@herschelgonzalez.com>
2023-02-26 21:00:34 -0600

    corregir vista en constraint

--- Calculadora/app/src/main/java/com/practica/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/practica/calculadora/MainActivity.java
@@ -3,12 +3,26 @@ package com.practica.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button cero,uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve;
+    EditText pantalla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    public static void setTextOnScreen(String text){
+
+    }
+
+
 }
